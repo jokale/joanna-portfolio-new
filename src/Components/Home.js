eca8b35554dd299c6b1271a576ee3369bb208b71
jokale <kalemajoanna@gmail.com>
2020-11-20 14:38:36 +0000

    change headers to new font

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,8 +5,10 @@ class Home extends React.Component{
     render(){
         return(
             <div>
-                <h1>Joanna Kalema</h1>
-                <h4>Web developer</h4>
+                <center>
+                 <h1>Joanna Kalema</h1>
+                 <h4>Web developer</h4>
+                </center>
             </div>
         )
     }
