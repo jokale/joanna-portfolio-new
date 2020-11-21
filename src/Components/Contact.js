762c45dd394f6c663a93b8230642625414e7db32
jokale <kalemajoanna@gmail.com>
2020-11-21 12:58:03 +0000

    create contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,7 +5,14 @@ class Contact extends React.Component{
     render(){
         return(
             <div>
+                <center>
                 <h1>Contact</h1>
+                <p>Want me to build something for you?</p>
+                <h2>Email me at </h2>
+                <a href="mailto:kalemajoanna@gmail.com?subject=New idea">Email Me</a>  
+
+
+                </center>
             </div>
         )
     }
