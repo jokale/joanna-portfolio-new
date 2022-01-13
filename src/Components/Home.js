56555a4a0fcdc56f085812775b43fd408fc8eeec
joannaWorkl <kalemajoanna@gmail.com>
2022-01-13 12:15:24 +0000

    import styled comps and react router

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 
-
-class Home extends React.Component{
-    render(){
-        return(
-            <div id="home">
-                <center>
-                 <h1>My name is Joanna Kalema</h1>
-                 <h4>I am a web developer</h4>
-                 <h5><a href="http://localhost:3000/about">Check out my story</a></h5>
-                </center>
-            </div>
-        )
-    }
+class Home extends React.Component {
+  render() {
+    return (
+      <div id="home">
+        <center>
+          <h1>Joanna Kalema</h1>
+          <h4>frontend developer</h4>
+          <h5>
+            <a href="http://localhost:3000/about">Check out my story</a>
+          </h5>
+        </center>
+      </div>
+    );
+  }
 }
 
-export default Home 
+export default Home;
+
+// I need to install react router, styled components, framer motion
